53cb940c916980a6116c57325192a3b3278650d3
ViktorKarsakov <vitakarsak@mail.ru>
2025-10-24 10:59:12 +0700

    update

--- JAVA/dz/dz_crud_operation/src/main/java/com/example/dz_crud_operation/controller/StudentsController.java
+++ JAVA/dz/dz_crud_operation/src/main/java/com/example/dz_crud_operation/controller/StudentsController.java
@@ -6,15 +6,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentsController {
     List<Students> students;
+    int nextId = 1;
 
     public StudentsController() {
         students = new ArrayList<>();
+    }
+
+    @GetMapping("/add")
+    public String add(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "surname") String surname,
+            @RequestParam(name = "age") int age,
+            @RequestParam(name = "phone", required = false) String phone,
+            @RequestParam(name = "email") String email
+    ){
+        name = name.trim();
+        surname = surname.trim();
+        email = email.trim().toLowerCase();
+        phone = (phone == null) ? null : phone.trim();
+
+        if(name.isEmpty() || surname.isEmpty() || email.isEmpty()){
+            return "Ошибка: имя, фамилия или email не могут быть пустыми";
+        }
+        int at = email.indexOf('@');
+        int dot =  email.lastIndexOf('.');
+        if(at <= 0 || dot <= at + 1 || dot >= email.length()){
+            return "Неправильный формат email";
+        }
+
     }
 
     @GetMapping("/all")
@@ -43,6 +67,25 @@ public class StudentsController {
         ).toList();
         return result;
     }
+
+    @GetMapping("/by-email")
+    public Students findByEmail(@RequestParam (name = "email") String email){
+        for (Students student : students) {
+            if (student.getEmail().trim().toLowerCase().equals(email.trim().toLowerCase())) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/filter-by-age")
+    public List<Students> filterByAge (@RequestParam(name = "min") int min, @RequestParam(name = "max") int max){
+        List<Students> result = students.stream().filter(student ->
+               student.getAge() >= min && student.getAge() <= max).toList();
+        return result;
+    }
+
+
 }
 
 
