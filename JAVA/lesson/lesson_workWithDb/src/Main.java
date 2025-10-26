6bf92861435e33d57632cb563bcdf95bf4b5adbf
ViktorKarsakov <vitakarsak@mail.ru>
2025-10-26 11:10:16 +0700

    update

---
+++ JAVA/lesson/lesson_workWithDb/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //JDBC (Java Database Connect)
+
+        //connect database
+        //send sql
+        //receive result
+
+        //connector -> lib (jar)
+    }
+}
